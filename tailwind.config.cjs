4f38b5c114ae72932b70fe5601d2a0d953490006
Rodrigo Freitas <rodrigommfreitas@protonmail.com>
2023-02-03 20:28:42 +0000

    Add custom colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          light: "#71767B",
+          medium: "#202327",
+          dark: "#16181C",
+        },
+        blue: {
+          light: "#1D9BF0",
+          primary: "#1D9BF0",
+        },
+      },
+    },
   },
   plugins: [],
 };
