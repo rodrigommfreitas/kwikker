310675e264abca90c9b4da512f5c6e273213f1b8
Rodrigo Freitas <rodrigommfreitas@protonmail.com>
2023-02-03 20:26:52 +0000

    Use app dir

--- next.config.mjs
+++ next.config.mjs
@@ -8,11 +8,16 @@
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
+  experimental: {
+    appDir: true,
+  },
   /* If trying out the experimental appDir, comment the i18n config out
    * @see https://github.com/vercel/next.js/issues/41980 */
+  /*
   i18n: {
     locales: ["en"],
     defaultLocale: "en",
   },
+  */
 };
 export default config;
