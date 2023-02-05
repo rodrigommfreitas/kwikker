c1236ad0a2a044e01ec381326dfdcd00b7059d7d
Rodrigo Freitas <rodrigommfreitas@protonmail.com>
2023-02-05 15:20:00 +0000

    Finish feed page header UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,92 +1,62 @@
+"use client";
 import { type NextPage } from "next";
+import { useState } from "react";
+import { CreateKweek } from "../components/shared/CreateKweek/CreateKweek";
 
 const Home: NextPage = () => {
-  // justify-between
+  const [activeTab, setActiveTab] = useState<"forYou" | "following">("forYou");
+
+  const handleClick = (tab: string) => {
+    tab === "following" ? setActiveTab("following") : setActiveTab("forYou");
+  };
+
   return (
-    <main className="items-center gap-8 py-3 px-3">
-      <span>feed</span>
-      <br />
-      <span>feed</span>
-      <br />
-      <span>feed</span>
-      <br />
-      <span>feed</span>
-      <br />
-      <span>feed</span>
-      <br />
-      <span>feed</span>
-      <br />
-      <span>feed</span>
-      <br />
-      <span>feed</span>
-      <br />
-      <span>feed</span>
-      <br />
-      <span>feed</span>
-      <br />
-      <span>feed</span>
-      <br />
-      <span>feed</span>
-      <br />
-      <span>feed</span>
-      <br />
-      <span>feed</span>
-      <br />
-      <span>feed</span>
-      <br />
-      <span>feed</span>
-      <br />
-      <span>feed</span>
-      <br />
-      <span>feed</span>
-      <br />
-      <span>feed</span>
-      <br />
-      <span>feed</span>
-      <br />
-      <span>feed</span>
-      <span>feed</span>
-      <br />
-      <span>feed</span>
-      <br />
-      <span>feed</span>
-      <br />
-      <span>feed</span>
-      <br />
-      <span>feed</span>
-      <br />
-      <span>feed</span>
-      <br />
-      <span>feed</span>
-      <br />
-      <span>feed</span>
-      <br />
-      <span>feed</span>
-      <br />
-      <span>feed</span>
-      <br />
-      <span>feed</span>
-      <br />
-      <span>feed</span>
-      <br />
-      <span>feed</span>
-      <br />
-      <span>feed</span>
-      <br />
-      <span>feed</span>
-      <br />
-      <span>feed</span>
-      <br />
-      <span>feed</span>
-      <br />
-      <span>feed</span>
-      <br />
-      <span>feed</span>
-      <br />
-      <span>feed</span>
-      <br />
-      <span>feed</span>
-    </main>
+    <>
+      <header className="sticky top-0 h-28 border-b border-gray-medium backdrop-blur-md">
+        <button className="flex h-1/2 w-full items-center pl-4 text-xl font-bold">
+          Home
+        </button>
+        <div className="flex h-1/2 text-gray-light">
+          <button
+            onClick={() => handleClick("forYou")}
+            className="flex w-full justify-center transition hover:bg-gray-medium/50"
+          >
+            <div
+              className={` ${
+                activeTab === "forYou"
+                  ? "border-b-2 border-blue-light font-semibold text-white"
+                  : "font-gray-light font-normal"
+              } flex h-14 w-fit items-center`}
+            >
+              For you
+            </div>
+          </button>
+          <button
+            onClick={() => handleClick("following")}
+            className="flex w-full justify-center transition hover:bg-gray-medium/50 "
+          >
+            <div
+              className={` ${
+                activeTab === "following"
+                  ? "border-b-2 border-blue-light font-semibold text-white"
+                  : "font-gray-light font-normal"
+              } flex h-14 w-fit items-center`}
+            >
+              Following
+            </div>
+          </button>
+        </div>
+      </header>
+      <CreateKweek />
+      <CreateKweek />
+      <CreateKweek />
+      <CreateKweek />
+      <CreateKweek />
+      <CreateKweek />
+      <CreateKweek />
+      <CreateKweek />
+      <CreateKweek />
+    </>
   );
 };
 
