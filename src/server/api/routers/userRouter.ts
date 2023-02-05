def6f398624ad73891f2eb1bf197765174475a78
Rodrigo Freitas <rodrigommfreitas@protonmail.com>
2023-02-05 22:56:46 +0000

    Create user router

---
+++ src/server/api/routers/userRouter.ts
@@ -0,0 +1,26 @@
+import { z } from "zod";
+
+import { createTRPCRouter, publicProcedure, protectedProcedure } from "../trpc";
+
+export const userRouter = createTRPCRouter({
+  getUser: publicProcedure
+    .input(z.object({ id: z.string() }))
+    .query(async ({ ctx, input }) => {
+      try {
+        const user = await ctx.prisma.user.findUnique({
+          where: { id: input.id },
+        });
+        return user;
+      } catch (err) {
+        console.log(`It wasn't possible to find user...\n ${err as string}`);
+      }
+    }),
+
+  getAll: publicProcedure.query(({ ctx }) => {
+    return ctx.prisma.example.findMany();
+  }),
+
+  getSecretMessage: protectedProcedure.query(() => {
+    return "you can now see this secret message!";
+  }),
+});
