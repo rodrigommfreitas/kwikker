57e5b416f5f3efd99a46831396590d6430a9e809
Rodrigo Freitas <rodrigommfreitas@protonmail.com>
2023-02-05 17:32:30 +0000

    .

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import { useSession } from "next-auth/react";
 import { BiPencil } from "react-icons/bi";
 import { Nav } from "./Nav";
@@ -10,7 +9,7 @@ export const Sidebar = () => {
   return (
     <div className="sticky top-0 mx-1 flex h-screen flex-col items-center xl:mx-6 xl:items-baseline">
       <Nav />
-      {!session && (
+      {session && (
         <button className="mt-4 rounded-full bg-blue-light p-3 font-bold transition hover:bg-blue-light/90 xl:px-24 xl:py-[14px]">
           <i className="flex h-fit w-fit xl:hidden">
             <BiPencil size={"1.5em"} />
