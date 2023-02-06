5680d2079e3b900f779c19515ae0569662d1f8bf
Rodrigo Freitas <rodrigommfreitas@protonmail.com>
2023-02-06 20:19:31 +0000

    Add next image configuration

--- next.config.mjs
+++ next.config.mjs
@@ -19,5 +19,16 @@ const config = {
     defaultLocale: "en",
   },
   */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: "",
+        pathname: "/a/**",
+      },
+    ],
+  },
 };
+
 export default config;
