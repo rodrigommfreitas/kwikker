265d0e77a2176a5020917571d3e05522cc96df3c
Rodrigo Freitas <rodrigommfreitas@protonmail.com>
2023-02-18 11:10:35 +0000

    Add replies

--- src/components/shared/KweekPost.tsx
+++ src/components/shared/KweekPost.tsx
@@ -5,22 +5,26 @@ import { useState } from "react";
 import { api } from "../../utils/api";
 import { BiDotsHorizontalRounded } from "react-icons/bi";
 import { FaRegComment } from "react-icons/fa";
-import { AiOutlineRetweet, AiOutlineHeart } from "react-icons/ai";
+import { AiOutlineRetweet, AiOutlineHeart, AiFillHeart } from "react-icons/ai";
 import { RiShareForward2Fill } from "react-icons/ri";
 import { useRouter } from "next/navigation";
 import { useSession } from "next-auth/react";
 
 type Props = {
   kweek: Kweek;
-  type: "feed" | "reply" | "page";
   rekweekerId?: string;
   rekweekerUsername?: string;
+  replyTo?: string;
+  setLikes?: React.Dispatch<React.SetStateAction<Kweek[]>>;
 };
 
-// Make this component responsive
-// Make UI adapt to different types of kweeks
-
-export const KweekPost = ({ kweek, rekweekerId, rekweekerUsername }: Props) => {
+export const KweekPost = ({
+  kweek,
+  rekweekerId,
+  rekweekerUsername,
+  replyTo,
+  setLikes,
+}: Props) => {
   const [user, setUser] = useState<User | null>(null);
   const [isLiked, setIsLiked] = useState<boolean>(false);
   const [isRekweeked, setIsRekweeked] = useState<boolean>(false);
@@ -34,7 +38,7 @@ export const KweekPost = ({ kweek, rekweekerId, rekweekerUsername }: Props) => {
   const router = useRouter();
 
   const { data: userData, isLoading } = api.user.getUser.useQuery(
-    { id: kweek.authorId },
+    { id: kweek?.authorId },
     {
       onSuccess(userData: User) {
         setUser(userData);
@@ -43,7 +47,7 @@ export const KweekPost = ({ kweek, rekweekerId, rekweekerUsername }: Props) => {
   );
 
   const { data: stats } = api.kweek.getStats.useQuery(
-    { id: kweek.id },
+    { id: kweek?.id },
     {
       onSuccess(stats: {
         replyCount: number;
@@ -58,7 +62,7 @@ export const KweekPost = ({ kweek, rekweekerId, rekweekerUsername }: Props) => {
   );
 
   const { data: isRekweekedByUser } = api.kweek.getIsRekweekedByUser.useQuery(
-    { kweekId: kweek.id, userId: session?.user.id as string },
+    { kweekId: kweek?.id, userId: session?.user.id as string },
     {
       onSuccess(isRekweekedByUser: boolean) {
         setIsRekweeked(isRekweekedByUser);
@@ -67,7 +71,7 @@ export const KweekPost = ({ kweek, rekweekerId, rekweekerUsername }: Props) => {
   );
 
   const { data: isLikedByUser } = api.kweek.getIsLikedByUser.useQuery(
-    { kweekId: kweek.id, userId: session?.user.id as string },
+    { kweekId: kweek?.id, userId: session?.user.id as string },
     {
       onSuccess(isLikedByUser: boolean) {
         setIsLiked(isLikedByUser);
@@ -83,7 +87,7 @@ export const KweekPost = ({ kweek, rekweekerId, rekweekerUsername }: Props) => {
   });
 
   const { mutate: unRekweekKweek } = api.kweek.unRekweekKweek.useMutation({
-    onSuccess() {
+    onSuccess(unrekweek) {
       setRekweekCount(rekweekCount - 1);
       setIsRekweeked(false);
     },
@@ -93,6 +97,7 @@ export const KweekPost = ({ kweek, rekweekerId, rekweekerUsername }: Props) => {
     onSuccess() {
       setLikeCount(likeCount + 1);
       setIsLiked(true);
+      setLikes && setLikes((likes) => [...likes, kweek]);
     },
   });
 
@@ -100,6 +105,8 @@ export const KweekPost = ({ kweek, rekweekerId, rekweekerUsername }: Props) => {
     onSuccess() {
       setLikeCount(likeCount - 1);
       setIsLiked(false);
+      setLikes &&
+        setLikes((likes) => likes.filter((like) => like.id !== kweek.id));
     },
   });
 
@@ -111,7 +118,7 @@ export const KweekPost = ({ kweek, rekweekerId, rekweekerUsername }: Props) => {
     );
 
   return (
-    <button
+    <div
       className={`${
         rekweekerId && rekweekerUsername ? "pt-[6px]" : "pt-3"
       } flex h-fit w-full flex-col border-b border-gray-medium px-4 pb-3 text-left`}
@@ -125,40 +132,60 @@ export const KweekPost = ({ kweek, rekweekerId, rekweekerUsername }: Props) => {
           rekweeked
         </div>
       )}
+
       <div className="flex gap-3">
         <Image
           src={user?.image as string}
           alt=""
           width={48}
           height={48}
-          className="max-h-[48px] max-w-[48px] rounded-full bg-black"
+          className="max-h-[48px] max-w-[48px] cursor-pointer rounded-full bg-black"
           onClick={() => router.push(`/profile/${user?.id as string}`)}
         />
         <div className="h-fit w-full">
           <div className="flex justify-between">
             <div>
-              <a
-                onClick={() => router.push(`/profile/${user?.id as string}`)}
-                className="font-bold hover:underline"
-              >
-                {user?.name}
-              </a>
-              <span className="text-gray-light"> · </span>
-              <a href="#" className="text-gray-500 hover:underline">
-                {kweek.createdAt.toLocaleString("default", { day: "numeric" })}{" "}
-                {kweek.createdAt.toLocaleString("default", { month: "short" })}{" "}
-                {kweek.createdAt.getFullYear()}
-              </a>
+              <div>
+                <a
+                  onClick={() => router.push(`/profile/${user?.id as string}`)}
+                  className="cursor-pointer font-bold hover:underline"
+                >
+                  {user?.name}
+                </a>
+                <span className="text-gray-light"> · </span>
+                <a href="#" className="text-gray-500 hover:underline">
+                  {kweek.createdAt.toLocaleString("default", {
+                    day: "numeric",
+                  })}{" "}
+                  {kweek.createdAt.toLocaleString("default", {
+                    month: "short",
+                  })}{" "}
+                  {kweek.createdAt.getFullYear()}
+                </a>
+              </div>
+
+              {replyTo && (
+                <button className="my-1 text-gray-light">
+                  In reply to{" "}
+                  <span className="text-blue-primary">{replyTo}</span>
+                </button>
+              )}
             </div>
+
             <button className="h-fit w-fit rounded-full p-1 text-gray-light transition hover:bg-blue-light/20 hover:text-blue-light active:bg-blue-light/30">
               <BiDotsHorizontalRounded size={"1.25em"} />
             </button>
           </div>
 
-          <span>{kweek.content}</span>
+          <button
+            onClick={() => !replyTo && router.push(`/status/${kweek.id}`)}
+          >
+            {kweek.content}
+          </button>
 
           <div className="mt-3 flex w-11/12 max-w-md items-center justify-between text-gray-light sm:w-5/6 ">
             <button
+              onClick={() => !replyTo && router.push(`/status/${kweek.id}`)}
               onMouseEnter={() => setHoveredBtn("reply")}
               onMouseLeave={() => setHoveredBtn(null)}
               className="flex items-center gap-2 transition hover:text-blue-primary"
@@ -173,17 +200,19 @@ export const KweekPost = ({ kweek, rekweekerId, rekweekerUsername }: Props) => {
             </button>
             <button
               onClick={
-                isRekweeked
+                isRekweeked && !replyTo
                   ? () =>
                       unRekweekKweek({
                         kweekId: kweek.id,
                         rekweekerId: session?.user.id as string,
                       })
-                  : () =>
+                  : !replyTo
+                  ? () =>
                       rekweekKweek({
                         kweekId: kweek.id,
                         rekweekerId: session?.user.id as string,
                       })
+                  : () => null
               }
               onMouseEnter={() => setHoveredBtn("rekweek")}
               onMouseLeave={() => setHoveredBtn(null)}
@@ -201,17 +230,19 @@ export const KweekPost = ({ kweek, rekweekerId, rekweekerUsername }: Props) => {
             </button>
             <button
               onClick={
-                isLiked
+                isLiked && !replyTo
                   ? () =>
                       unLikeKweek({
                         kweekId: kweek.id,
                         likerId: session?.user.id as string,
                       })
-                  : () =>
+                  : !replyTo
+                  ? () =>
                       likeKweek({
                         kweekId: kweek.id,
                         likerId: session?.user.id as string,
                       })
+                  : () => null
               }
               onMouseEnter={() => setHoveredBtn("like")}
               onMouseLeave={() => setHoveredBtn(null)}
@@ -219,12 +250,21 @@ export const KweekPost = ({ kweek, rekweekerId, rekweekerUsername }: Props) => {
                 isLiked ? "text-red-500" : "text-gray-light"
               } flex items-center gap-2 transition hover:text-red-500`}
             >
-              <AiOutlineHeart
-                size={"1.75em"}
-                className={`rounded-full p-1 transition ${
-                  hoveredBtn === "like" ? "bg-red-500/20" : "bg-none"
-                }`}
-              />
+              {isLiked ? (
+                <AiFillHeart
+                  size={"1.75em"}
+                  className={`rounded-full p-1 transition ${
+                    hoveredBtn === "like" ? "bg-red-500/20" : "bg-none"
+                  }`}
+                />
+              ) : (
+                <AiOutlineHeart
+                  size={"1.75em"}
+                  className={`rounded-full p-1 transition ${
+                    hoveredBtn === "like" ? "bg-red-500/20" : "bg-none"
+                  }`}
+                />
+              )}
               <span className="text-sm">{likeCount}</span>
             </button>
             <button className="flex h-fit w-fit items-center rounded-full transition hover:bg-blue-light/20 hover:text-blue-primary">
@@ -233,6 +273,6 @@ export const KweekPost = ({ kweek, rekweekerId, rekweekerUsername }: Props) => {
           </div>
         </div>
       </div>
-    </button>
+    </div>
   );
 };
