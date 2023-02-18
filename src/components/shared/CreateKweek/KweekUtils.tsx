dcb5fc2d1606352c02d33b39d373e3e21ac9f99d
Rodrigo Freitas <rodrigommfreitas@protonmail.com>
2023-02-18 10:59:49 +0000

    Make component reusable between create kweek and reply to kweek

--- src/components/shared/CreateKweek/KweekUtils.tsx
+++ src/components/shared/CreateKweek/KweekUtils.tsx
@@ -2,7 +2,32 @@ import { AiOutlineCalendar, AiOutlineFileGif } from "react-icons/ai";
 import { BiImage, BiPoll, BiSmile } from "react-icons/bi";
 import { TiLocationOutline } from "react-icons/ti";
 
-export const KweekUtils = () => {
+type Props = {
+  reply?: boolean;
+};
+
+export const KweekUtils = ({ reply }: Props) => {
+  if (reply)
+    return (
+      <div className="flex h-fit items-center gap-1 text-blue-light">
+        <button className="rounded-full p-2 transition hover:bg-blue-light/10 active:bg-blue-light/20">
+          <BiImage size={"1.25em"} />
+        </button>
+        <button className="rounded-full p-2 transition hover:bg-blue-light/10 active:bg-blue-light/20">
+          <AiOutlineFileGif size={"1.25em"} />
+        </button>
+        <button className="rounded-full p-2 transition hover:bg-blue-light/10 active:bg-blue-light/20">
+          <BiSmile size={"1.25em"} />
+        </button>
+        <button
+          className="hidden rounded-full p-2 transition disabled:text-blue-light/50 sm:flex"
+          disabled
+        >
+          <TiLocationOutline size={"1.25em"} />
+        </button>
+      </div>
+    );
+
   return (
     <div className="ml-1 flex h-fit items-center gap-1 text-blue-light">
       <button className="rounded-full p-2 transition hover:bg-blue-light/10 active:bg-blue-light/20">
@@ -21,7 +46,7 @@ export const KweekUtils = () => {
         <AiOutlineCalendar size={"1.25em"} />
       </button>
       <button
-        className="hidden rounded-full p-1 transition disabled:text-blue-light/50 sm:flex"
+        className="hidden rounded-full p-2 transition disabled:text-blue-light/50 sm:flex"
         disabled
       >
         <TiLocationOutline size={"1.25em"} />
